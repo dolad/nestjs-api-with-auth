fe7b6ef5a9d26f667d97fecf188b41dc6da90b0d
David Akande <davidakande@Davids-MacBook-Pro.local>
2023-01-14 23:40:21 +0000

    added the env

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,7 @@ import { Environment } from './interface';
 
 export default () => ({
   app: {
-    port: parseInt(process.env.APP_PORT, 10) || 3000,
+    port: parseInt(process.env.APP_PORT, 10),
     environment: (process.env.APP_ENV) || Environment.DEVELOPMENT,
   },
 });
