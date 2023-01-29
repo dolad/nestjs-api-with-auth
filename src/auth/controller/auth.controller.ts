13bd366236d87df4d38bba1c4c0b653dc6600c89
David Akande <davidakande@Davids-MacBook-Pro.local>
2023-01-29 21:04:31 +0000

    hotfix

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Post, UseGuards, Request, Param, Get, Req } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
-import { User } from "src/storage/postgres/user.schema";
 import { IResponseMessage, wrapResponseMessage } from "src/utils/response.map";
 import { LoginDTO } from "../dtos/login.dto";
 import { RegistrationDTO } from "../dtos/registration.dto";
@@ -9,7 +8,6 @@ import { ResendRegistationDTO } from "../dtos/resendRegistration.dto";
 import { JwtAuthGuard } from "../guards/jwt-auth.guard";
 import { LocalAuthGuard } from "../guards/local-auth.guard";
 import { AuthService } from "../services/auth.services";
-import { LoginOutput } from "../types/loginOut.type";
 
 
 @Controller('auth')
