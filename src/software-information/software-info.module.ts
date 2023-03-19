889872d8e2c839266606657f9743d000cc893170
David Akande <davidakande@Davids-MacBook-Pro.local>
2023-03-19 00:24:41 +0000

    fixing

--- src/software-information/software-info.module.ts
+++ src/software-information/software-info.module.ts
@@ -4,9 +4,10 @@ import { softwareInfoProviders} from "./providers/provider";
 import { SoftwareInformationServices } from "./services/software-info.services";
 import { RutterServices } from "./services/rutter.service";
 import { BusinessEntityModule } from "src/business-entity/business.module";
+import { UserModule } from "src/user/user.module";
 
 @Module({
-    imports: [BusinessEntityModule],
+    imports: [BusinessEntityModule, UserModule],
     controllers: [SoftwareInfoController],
     providers: [SoftwareInformationServices, RutterServices, ...softwareInfoProviders],
     exports: [SoftwareInformationServices],
